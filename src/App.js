11089a5746510309ac54fafd6a87e2cfd91da7a9
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2022-12-22 17:25:15 -0500

    Commit After recording

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Parallax pages={2} style={{ top: '0', left: '0' }} class="keyart">
+      <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
         <ParallaxLayer offset={0} speed={0.25}>
-          <div class="keyart_layer parallax" id="artback"></div>
+          <div class="animation_layer parallax" id="artback"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.3}>
-          <div class="keyart_layer parallax" id="mountain"></div>
+          <div class="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={-0.1}>
-          <div class="keyart_layer parallax" id="logoland"></div>
+          <div class="animation_layer parallax" id="logoland"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.3}>
-          <div class="keyart_layer parallax" id="jungle1"></div>
+          <div class="animation_layer parallax" id="jungle1"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.35}>
-          <div class="keyart_layer parallax" id="jungle2"></div>
+          <div class="animation_layer parallax" id="jungle2"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.5}>
-          <div class="keyart_layer parallax" id="jungle3"></div>
+          <div class="animation_layer parallax" id="jungle3"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.45}>
-          <div class="keyart_layer parallax" id="jungle4"></div>
+          <div class="animation_layer parallax" id="jungle4"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.40}>
-          <div class="keyart_layer parallax" id="manonmountain"></div>
+          <div class="animation_layer parallax" id="manonmountain"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.35}>
-          <div class="keyart_layer parallax" id="jungle5"></div>
+          <div class="animation_layer parallax" id="jungle5"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.25}>
           <TextBlock />
